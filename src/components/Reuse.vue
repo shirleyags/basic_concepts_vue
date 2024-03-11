6a302549e3bf3012083414e907a5ea789de79176
ShirleyAlice <shirleyags@yahoo.com.br>
2024-03-10 21:10:43 -0300

    Utiliza v-show e eventos de click

---
+++ src/components/Reuse.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<button @click="ativar">Cliquei aqui!</button>
+		<span v-show="ativou">Ativado</span>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Reuse',
+		data() {
+			return {
+				ativou: false
+			}
+		},
+		methods: {
+			ativar() {
+				this.ativou = !this.ativou
+			}
+		}
+	}
+</script>
