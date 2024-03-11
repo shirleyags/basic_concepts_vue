e6999e15c1b6d78c37de0e62184548a86aaa925c
ShirleyAlice <shirleyags@yahoo.com.br>
2024-03-10 21:00:49 -0300

    Cria Lifecycle hooks

---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+  export default ({
+    name: 'LifeCycle',
+    data() {
+      return {
+          nome: 'Shirley Alice'
+      }
+    },
+    created() {
+      setTimeout(() => {
+            this.nome = 'Matheus'
+      }, 2000)
+    },
+    mounted() {
+      setTimeout(() => {
+            this.nome = 'Pedro'
+      }, 1000)
+    }    
+  })
+</script>
