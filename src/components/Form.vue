6d046aef97557db08177b8dbe4b3fd4b7f69cb02
ShirleyAlice <shirleyags@yahoo.com.br>
2024-03-10 21:09:59 -0300

    Utiliza v-model

---
+++ src/components/Form.vue
@@ -0,0 +1,41 @@
+<template>
+	<form action="" @submit="enviarFormulario($event)">
+		<div>
+			<input type="text" v-model="name">
+		</div>
+		<div>
+			<input type="text" v-model="email">
+		</div>
+		<div>
+			<Submit />
+		</div>
+	</form>
+</template>
+
+<script>
+	import InputText from './form/InputText.vue'
+	import Submit from './form/Submit.vue'
+	export default {
+		name:'Form',
+		components: {
+			InputText,
+			Submit
+		},
+		data() {
+			return {
+				name: "",
+				email: ""
+			}
+		},
+		methods: {
+			enviarFormulario(e) {
+				e.preventDefault()
+
+				const name = this.name
+				const email = this.email
+				console.log(name)
+				console.log(email)
+			}
+		}
+	}
+</script>
