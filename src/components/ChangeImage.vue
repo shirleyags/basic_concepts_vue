04234437dab6e85af5ad253c205212b240b59404
ShirleyAlice <shirleyags@yahoo.com.br>
2024-03-10 21:03:18 -0300

    Utiliza emit

---
+++ src/components/ChangeImage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <button @click="$emit('changeImage')">Trocar imagem do perfil</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ChangeImage',
+    emits: ['changeImage']
+  }
+</script>
