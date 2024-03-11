d454dee4ce20c97933d0929bb30651643365633f
ShirleyAlice <shirleyags@yahoo.com.br>
2024-03-10 21:06:22 -0300

    Importação e componentes

---
+++ src/components/Person.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <h2>Essa é a descrição do: {{ nome }}</h2>
+  <Info  :email="email" />
+  <Form />
+</div>
+</template>
+
+<script>
+  import Info from './Info.vue'
+  import Form from './Form.vue'
+
+  export default {
+    name: 'Person',
+    components: {
+      Info,
+      Form
+    },
+    data() {
+      return {
+        nome: 'Alice',
+        email: "shirley@gmail.com"
+      }
+    }
+  }
+</script>
