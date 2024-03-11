99130518e60bdfb73429e5c1199278d5d19db583
ShirleyAlice <shirleyags@yahoo.com.br>
2024-03-10 21:08:43 -0300

    Utiliza o emit

---
+++ src/components/Picture.vue
@@ -0,0 +1,25 @@
+<template>
+	<img :src="avatar" :alt="descricao">
+	<ChangeImage @change-image="changeOtherImage"/>
+</template>
+
+<script>
+	import ChangeImage from './ChangeImage.vue'
+	export default {
+		name: 'Picture',
+		components: { 
+			ChangeImage 
+		},
+		data() {
+			return {
+				avatar: "/img/avatar.png",
+				descricao: "Shirley Alice"
+			}
+		},
+		methods: {
+			changeOtherImage() {
+				this.avatar = "/img/avatar2.png"
+			}
+		}
+	}
+</script>
