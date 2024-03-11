58cdf300b628f8e2050f28115cb64d9d675d4468
ShirleyAlice <shirleyags@yahoo.com.br>
2024-03-10 21:02:34 -0300

    Cria componente e aplica escopo local para o css

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+	<ul>
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Projetos</a></li>
+		<li><a href="#">Contato</a></li>
+	</ul>
+    
+</template>
+
+<script>
+	export default {
+		name:'Header'
+	}
+	</script>
+
+	<!-- Exemplo de CSS local -->
+	<style scoped>
+		ul {
+			list-style: none;
+			display: flex;
+			background: #999;
+		}
+		li {
+			margin-right: 10px;
+		}
+		a {
+			color: #DDD;
+			text-decoration: none;
+			transition: .5s;
+		}
+		a:hover {
+			color: #40E0D0;
+		}
+</style>
