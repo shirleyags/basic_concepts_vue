ffd145de498409746b6f230d8c4e6551e9f49df7
ShirleyAlice <shirleyags@yahoo.com.br>
2024-03-10 21:01:51 -0300

    Multiplos eventos

---
+++ src/components/MultipleEvents.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <button @click="primeiro($event), segundo($event), terceiro($event)">Ativar múltiplos eventos</button>
+    <p>{{ multiplos_eventos }}</p>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'MultipleEvents',
+    data() {
+      return {
+        multiplos_eventos: ""
+      }
+    },
+    methods: {
+      primeiro(e) {
+        this.multiplos_eventos = "Primeiro evento!"
+      },
+      segundo(e) {
+        setTimeout(() => this.multiplos_eventos = "Segundo evento!", 500)
+      },
+      terceiro(e) {
+        setTimeout(() => this.multiplos_eventos = "", 1500)
+      }
+    }
+  }
+</script>
